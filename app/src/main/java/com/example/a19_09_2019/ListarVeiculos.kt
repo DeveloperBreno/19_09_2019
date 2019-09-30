425c17451f465896622a42f0932a4fcdba1e2a61
breno <developerbreno@gmail.com>
2019-09-29 22:42:59 -0300

    tela listar cliente e veiculo

---
+++ app/src/main/java/com/example/a19_09_2019/ListarVeiculos.kt
@@ -0,0 +1,53 @@
+package com.example.a19_09_2019
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.dashboard.*
+
+
+
+class ListarVeiculos : DebugActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.listarveiculos)
+
+
+        val params = intent.extras
+        val nome = params?.getString("nome_usuario")
+        val n = params?.getString("numero")
+
+        supportActionBar?.title = "Cadastrar cliente"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id= item?.itemId
+
+        if (id == R.id.action_buscar){
+            Toast.makeText(this, "clicou em buscar",
+                Toast.LENGTH_SHORT).show()
+
+        } else if (id == R.id.action_atualizar){
+            Toast.makeText(this, "clicou em atualizar",
+                Toast.LENGTH_SHORT).show()
+
+
+        }else if (id == android.R.id.home)
+            finish()
+
+
+        return super.onOptionsItemSelected(item)
+    }
+}
