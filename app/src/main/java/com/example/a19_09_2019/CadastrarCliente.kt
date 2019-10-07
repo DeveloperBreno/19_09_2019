b96a5b301187337e923e290ec48674fd9601d45f
breno <developerbreno@gmail.com>
2019-10-07 18:35:49 -0300

    bruno

--- app/src/main/java/com/example/a19_09_2019/CadastrarCliente.kt
+++ app/src/main/java/com/example/a19_09_2019/CadastrarCliente.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import com.example.systemengineerdown.R
 import kotlinx.android.synthetic.main.cadastrarcliente.*
 
 
